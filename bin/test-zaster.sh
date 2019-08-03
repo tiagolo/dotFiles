9a67e751ec8bb86ef075e5bae3f345a5c49a49ca
Tiago Lopes da Costa <tiago.costa@mv.com.br>
2019-08-02 23:06:42 -0300

    Test Zaster

--- bin/test-zaster.sh
+++ bin/test-zaster.sh
@@ -3,10 +3,11 @@
 COUNT=0
 
 while true; do
-  COUNT=COUNT+1;
-  firefox --new-tab 'https://zasterr.com/dJoAEXO';
-  sleep 16;  
-  i3-msg '[class=firefox]' focus
+  COUNT=$((COUNT+1));
+  firefox --new-tab 'https://zasterr.com/dJoAEXO'
+  # echo "waiting";
+  sleep 20;  
+  i3-msg '[class=firefox]' focus;
   xdotool key Ctrl+w;
 
   echo "Contador execuções Zaster: $COUNT"
